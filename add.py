239f1ecbce32ba88ac95e4ff0869f29f033ed26b
Gopika <gopika@Shanmugas-Air.lan>
2022-11-01 10:04:23 -0400

    tree

--- add.py
+++ add.py
@@ -5,7 +5,11 @@ print(2+3)
 a = 55
 b = int(input("enter a num:"))
 x = a + b
-print(x)
 
-print("hello")
-print("im sleepy")
+print(a+ b)
+
+tree = int(input("how many tree do you see?:"))
+if tree == 1:
+    print("plant 10tree")
+else:
+    print("plant 5 tree")
